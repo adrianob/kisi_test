c4bcb0141da1476e38b3b9ab043273b6e9758171
Adriano Benin <adriano.benin@gmail.com>
2018-08-31 14:21:49 -0300

    move to morgue queue on third attempt

--- app/jobs/test_job.rb
+++ app/jobs/test_job.rb
@@ -6,9 +6,15 @@ end
 
 class TestJob < ApplicationJob
   queue_as :default
-  retry_on StandardError, wait: 5.minutes, attempts: 2, queue: :default
+  retry_on StandardError, wait: 5.minutes, attempts: 3, queue: :default
 
   def perform(*args)
+    if executions == 3
+      Rails.logger.info "Final try, moving to morgue queue"
+      retry_job wait: 0, queue: :morgue
+      return nil
+    end
+
     Rails.logger.info "Started test job with args " +
                       "#{args}"
     ExampleService.call 123
