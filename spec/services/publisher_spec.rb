b2095778edd64d121d2253b4902a0a4493bcf99a
Adriano Benin <adriano.benin@gmail.com>
2018-09-03 08:28:46 -0300

    update specs

--- spec/services/publisher_spec.rb
+++ spec/services/publisher_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 RSpec.describe Publisher do
 
-  let(:publish) { TestJob.perform_later('Hello, World!', 'TestJob', ['firstTopic', 'secondTopic']) }
+  let(:publish) { TestJob.perform_later('Hello, World!', ['firstTopic', 'secondTopic']) }
 
   before do
     expect_any_instance_of(Publisher).to receive(:publish_to_topics)
